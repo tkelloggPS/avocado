221923c962f0ce78ec3c92862005de0d504ab43c
Tim Kellogg <tkellogg@peerstreet.com>
2020-03-28 13:22:58 -0700

    Add quick dynamic react routing with steps

--- app/javascript/packs/components/eligibility_form.jsx
+++ app/javascript/packs/components/eligibility_form.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from "react"
-import { Form } from "semantic-ui-react"
+import { Form, Button } from "semantic-ui-react"
+import { Header } from 'semantic-ui-react'
 
-export default () => {
+export default ({ setStep }) => {
   const [businessAge, setBusinessAge] = useState(false)
   const [businessSize, setBusinessSize] = useState(false)
+  const [businessType, setBusinessType] = useState(false)
+
+  const handleSubmit = () => {
+    const valid = [businessAge, businessType, setBusinessSize].every(v => v)
+
+    if (valid) {
+      setStep(2)
+
+    } else {
+
+    }
+  }
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit} className="ui segment">
+      <Header as="h2">Eligibility</Header>
+
       <Form.Group inline>
         <label>Has your business been around for more than 1 year?</label>
         <Form.Radio
@@ -38,6 +53,23 @@ export default () => {
           onClick={() => setBusinessSize(false)}
         />
       </Form.Group>
+
+      <Form.Group inline>
+        <label>Does your business exclude marijuana, gambling or adult entertainment?</label>
+        <Form.Radio
+          label="True"
+          checked={businessType === true}
+          value={true}
+          onClick={() => setBusinessType(true)}
+        />
+        <Form.Radio
+          label="False"
+          checked={businessType === false}
+          value={false}
+          onClick={() => setBusinessType(false)}
+        />
+      </Form.Group>
+      <Button type='submit'>Submit</Button>
     </Form>
   )
 }
--- app/javascript/packs/components/index.jsx
+++ app/javascript/packs/components/index.jsx
@@ -1,20 +1,55 @@
-import React from "react"
-import Detail from "./detail"
+import React, { useState } from "react"
+import { Container, Step, Button } from 'semantic-ui-react'
+
 import EligibilityForm from "./eligibility_form"
-import NotEligible from "./not_eligible"
-import Notice from "./notice"
 import Provider from "./provider"
-import { Container } from 'semantic-ui-react'
 
 export default () => {
+  const [step, setStep] = useState(1)
+
+  const activeStep = () => {
+    switch (step) {
+      case 1:
+        return <EligibilityForm setStep={setStep} />
+        break;
+      case 2:
+        return <Provider setStep={setStep} />
+      case 3:
+        return <Download setStep={setStep} />
+      default:
+        return <EligibilityForm setStep = { setStep } />
+        break;
+    }
+  }
+
   return (
-    <Container>
-      <EligibilityForm />
-      <Detail />
-      <NotEligible />
-      <Notice message="success" />
-      <Notice message="failure" />
-      <Provider />
+    <Container className="ui center aligned padded">
+      <Step.Group ordered>
+        <Step active={step === 1} completed={step > 1}>
+          <Step.Content>
+            <Step.Title>Verify</Step.Title>
+            <Step.Description>Determine if your business if eligible</Step.Description>
+          </Step.Content>
+        </Step>
+
+        <Step active={step === 2} completed={step > 2}>
+          <Step.Content>
+            <Step.Title>Process</Step.Title>
+            <Step.Description>Upload accounting details</Step.Description>
+          </Step.Content>
+        </Step>
+
+        <Step active={step === 3} completed={step > 3}>
+          <Step.Content>
+            <Step.Title>Download</Step.Title>
+            <Step.Description>Print loan application</Step.Description>
+          </Step.Content>
+        </Step>
+      </Step.Group>
+
+      {activeStep()}
+
+      <Button disabled={step === 1} click={() => setStep(step - 1)}>Back</Button>
     </Container>
   )
 }

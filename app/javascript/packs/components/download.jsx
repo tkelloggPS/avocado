ddd788bf63193c8393ad2d3cf0997d92c21022c3
Tim Kellogg <tkellogg@peerstreet.com>
2020-03-29 05:18:52 -0700

    Add first pass at download

--- app/javascript/packs/components/download.jsx
+++ app/javascript/packs/components/download.jsx
@@ -1,29 +1,61 @@
 import React, { useState} from "react"
-import { Segment, Header, Button } from "semantic-ui-react"
+import {
+  Segment,
+  Header,
+  Button,
+  Dimmer,
+  Loader,
+  Image
+} from "semantic-ui-react"
+import axios from "axios"
 
 export default ({ setStep, step }) => {
   const [hasFailed, setHasFailed] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleSubmit = () => {
-    // TODO: implement download
-    setStep(4)
+    setHasFailed(false)
+    setIsLoading(true)
+
+    axios({
+      url: "/quickbooks",
+      method: 'GET',
+      responseType: 'blob',
+    }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]))
+      const link = document.createElement('a')
+
+      link.href = url
+      link.setAttribute('download', 'application.pdf')
+      document.body.appendChild(link)
+      link.click()
+
+      setIsLoading(false)
+      setStep(4)
+    }).catch(err => {
+      setHasFailed(true)
+      setIsLoading(false)
+    })
   }
 
   const message = () => {
     if (!hasFailed) return
 
-    return <Message><p>You must choose a provider</p></Message>
+    return <Message><p>We could not generate the download. Please try again</p></Message>
   }
 
   return (
     <Segment>
-      <Header as="h1">Download Your Application</Header>
+      <Dimmer active={isLoading} inverted>
+        <Loader inverted content='Loading' />
+      </Dimmer>
+
+      <Header as="h1">Download Your SBA Loan Application</Header>
 
       {message()}
 
-      <div className="button-group">
+      <div className="ui center aligned">
         <Button secondary onClick={handleSubmit}>Download</Button>
-        <Button onClick={() => setStep(step - 1)}>Back</Button>
       </div>
     </Segment>
   )
--- app/javascript/packs/components/success.jsx
+++ app/javascript/packs/components/success.jsx
@@ -3,9 +3,9 @@ import { Segment, Header, Button } from "semantic-ui-react"
 
 export default ({ setStep, step }) => (
   <Segment>
-    <Header as="h1">Success!</Header>
+    <Header as="h1" color="green">Success!</Header>
 
-    <p>We've generated an application for you below. You can download this file and submit to
+    <p>We've generated an application for you below. You can submit this form to
       <a href="https://www.sba.gov/funding-programs/disaster-assistance"> SBA</a>
     </p>
   </Segment>
